2513bb624eec265b16afe76bce8d1d030ac421cd
KarthikB-dev <55643522+KarthikB-dev@users.noreply.github.com>
2021-02-03 09:41:55 -0800

    Delete hop.java
    Switching to python for this project, I will make a hop class or use a data structure to store information about hops later.

--- hop.java
+++
@@ -1,6 +0,0 @@
-public class hop {
-    private int num;
-    private String ip;
-    //TODO add more fields, like the time associated with each hop
-    //todo add constructors, getters, and setters
-}
