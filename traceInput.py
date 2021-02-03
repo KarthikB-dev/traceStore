5354db9b28a296c942c8b7948bfada52da7f6e0b
KarthikB-dev <saberlight35@gmail.com>
2021-02-03 15:38:20 -0800

    traceInput displays the first line from trace.txt

--- traceInput.py
+++ traceInput.py
@@ -1,6 +1,10 @@
 import os
-path = "/home/impacable/Documents/traceStore/domain_input.sh"
-inputCommand = "sh " + path
+input_script_path = "/home/impacable/Documents/traceStore/domain_input.sh"
+inputCommand = "sh " + input_script_path
 runDomainInput = os.system(inputCommand)
 runDomainInput
-
+#start reading from the file
+trace_file = open("trace.txt")
+destination_info = trace_file.readline()
+print("Desination info:", destination_info)
+trace_file.close()
