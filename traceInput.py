a1538d61d51c1d1f448d790f4284e23f79eb788c
KarthikB-dev <saberlight35@gmail.com>
2021-02-03 18:43:55 -0800

    Stores each hop in an array

--- traceInput.py
+++ traceInput.py
@@ -1,10 +1,22 @@
 import os
+"""
 input_script_path = "/home/impacable/Documents/traceStore/domain_input.sh"
 inputCommand = "sh " + input_script_path
 runDomainInput = os.system(inputCommand)
 runDomainInput
+"""
 #start reading from the file
 trace_file = open("trace.txt")
 destination_info = trace_file.readline()
 print("Desination info:", destination_info)
-trace_file.close()
+
+hops = []
+hop = trace_file.readline()
+while hop:
+    hop = hop.rstrip('\n')
+    print("Hop:", hop)
+    hops.append(hop)
+    hop = trace_file.readline()
+
+
+
