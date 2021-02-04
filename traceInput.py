c701046500bffada71232843b3bfe94c35f761c9
KarthikB-dev <saberlight35@gmail.com>
2021-02-04 14:38:06 -0800

    added a function to find the IP addresses within each hop

--- traceInput.py
+++ traceInput.py
@@ -1,4 +1,5 @@
 import os
+import re
 """
 input_script_path = "/home/impacable/Documents/traceStore/domain_input.sh"
 inputCommand = "sh " + input_script_path
@@ -10,13 +11,29 @@ trace_file = open("trace.txt")
 destination_info = trace_file.readline()
 print("Desination info:", destination_info)
 
-hops = []
-hop = trace_file.readline()
-while hop:
-    hop = hop.rstrip('\n')
-    print("Hop:", hop)
-    hops.append(hop)
-    hop = trace_file.readline()
+curr_hops = []
+curr_hop = trace_file.readline()
+
+class hop:
+    index = 0
+    IPs = set()
+    full_name = ""
+    def __init__(self, index, IPs, full_name):
+        self.index = index
+        self.IPs = IPs
+        self.full_name = full_name
+    #functions to retrieve information about each curr_hop
+
+def getIPs(hop):
+    return set(re.findall(r"\b\d{1,3}\.\d{1,3}\.\d{1,3}\.\d{1,3}\b", hop))
+
+while curr_hop:
+    curr_hop = curr_hop.rstrip('\n')
+    print("curr_Hop:", curr_hop)
+    curr_hops.append(curr_hop)
+    print("IP address:", getIPs(curr_hop))
+    curr_hop = trace_file.readline()
+
 
 
 
